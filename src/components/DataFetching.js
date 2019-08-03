9ca9b2df230f8bca910752d26ad06004fcd9f345
mudrakpatel <mudrakpatel96@gmail.com>
2019-08-03 13:58:07 -0400

    Fetching data with useEffect - Part 1

---
+++ src/components/DataFetching.js
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+function DataFetching(){
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+          .then(response => {
+            setPosts(response.data);
+            console.log("Data fetched successfully.");
+            //console.log(response);
+          }).catch(error => {
+            console.log(error);
+          });
+  }, []);
+
+  return(
+    <div>
+      <ul>
+        {
+          posts.map(post => <li key={post.id}>{post.title}</li>)
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default DataFetching;
